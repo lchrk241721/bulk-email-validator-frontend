1f88ae2b32409ccf356c0bde4a4377b52a792899
Chaitanya Rajkumar Limmala <lchrk2417@aol.com>
2025-09-25 19:00:29 +0530

    Made changes to live backend server.

--- src/components/EmailValidator.jsx
+++ src/components/EmailValidator.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:3001/api/email';
+const API_BASE = 'https://bulk-email-validator-backend.onrender.com/api/email';
 
 const EmailValidator = ({ onValidationComplete, onValidationStart, onProgressUpdate, loading }) => {
   const [emails, setEmails] = useState('');
